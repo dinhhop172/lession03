03cc870585998ebc468b86485ebba437b4fbd691
keira172 <dinhhop172@gmail.com>
2020-05-08 22:24:43 +0700

    laravel

---
+++ database/migrations/2020_05_07_162908_create_order_detail_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderDetailTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_detail', function (Blueprint $table) {
+            $table->integer('order_id')->unsigned();
+            $table->integer('product_id')->unsigned();
+            $table->foreign('order_id')->references('id')->on('order');
+            $table->foreign('product_id')->references('id')->on('product');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_detail');
+    }
+}
